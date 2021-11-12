a2831c1ddb3e21636b6e0d889ce7fd7192c7302d
k-rod12 <kenleyrodriguez12@gmail.com>
2021-11-12 02:08:52 -0500

    testing

--- App.js
+++ App.js
@@ -1,17 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Icon } from "react-native-elements";
 
 export default function App() {
   return (
+
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+        <TouchableOpacity
+          style={{
+            alignSelf: "flex-start",
+            marginLeft: 15,
+          }}
+          onPress={() => {
+            props.navigation.pop();
+          }}
+        >
+          <Icon name="chevron-left" size={40} />
+        </TouchableOpacity>
+
+      <View>
+        <Text> Open up App.js to start working on your app!</Text>
+        
+        <StatusBar style="auto" />
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  backArrow:{
+    flex: 1,
+    backgroundColor: '#0000'
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
